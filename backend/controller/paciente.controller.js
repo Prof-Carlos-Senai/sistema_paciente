6d1d83a135ae50242a62a81d531d35dcd32092fd
Carlos <silva@edu.sc.senai.br>
2026-04-28 09:49:45 -0300

    consultar e apagar no front e back

--- backend/controller/paciente.controller.js
+++ backend/controller/paciente.controller.js
@@ -20,4 +20,37 @@ const listar = async (req,res) => {
     }
 }
 
-module.exports = { cadastrar, listar }
+const consultar = async (req,res) => {
+    const id = req.params.id
+    console.log(id)
+    try{
+        const dado = await Paciente.findOne({ where: {codPaciente: id}})
+        if(!dado){
+            res.status(404).json({message: 'Paciente não encontrado!'})
+        }else{
+            res.status(200).json(dado)
+        }
+    }catch(err){
+        res.status(500).json({message: 'Erro ao consultar o paciente!'})
+        console.error('Erro ao consultar o paciente!',err)
+    }
+}
+
+const apagar = async (req,res) =>{
+    const id = req.params.id
+    console.log(id)
+    try{
+        const dado = await Paciente.findOne({ where: {codPaciente: id}})
+        if(!dado){
+            res.status(404).json({message: 'Paciente não encontrado!'})
+        }else{
+            await Paciente.destroy({ where: {codPaciente: id}})
+            res.status(200).json({message: 'Paciente excluído com sucesso!'})
+        }
+    }catch(err){
+        res.status(500).json({message: 'Erro ao excluir o paciente!'})
+        console.error('Erro ao excluir o paciente!',err)
+    }
+}
+
+module.exports = { cadastrar, listar, consultar, apagar }
